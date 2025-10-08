b4d0b990f043a0d7df03a8f8e1cae8bab53b18c9
Hiwot Minda <www.hiwotminda3@gmail.com>
2025-10-08 17:26:09 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+// import logo from './logo.svg';
+// import './app.css';
+import Counter from './counter';
+function App(){
+    return(
+        <div className = "App">
+            <Counter />
+        </div>
+    );
+}
+export default App;
